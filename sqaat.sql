7740612785273bcb616705a3051035ac3fbe5fc5
Lalle <niklas.lavrell@gmail.com>
2013-06-10 21:00:50 +0800

    updated database

--- sqaat.sql
+++ sqaat.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Värd: localhost
--- Skapad: 06 jun 2013 kl 07:06
+-- Skapad: 10 jun 2013 kl 12:59
 -- Serverversion: 5.5.20
 -- PHP-version: 5.3.10
 
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `gunsmith` (
 --
 
 INSERT INTO `gunsmith` (`id`, `location`) VALUES
-(1, 'Harbin');
+(1, 0);
 
 -- --------------------------------------------------------
 
@@ -47,19 +47,34 @@ INSERT INTO `gunsmith` (`id`, `location`) VALUES
 
 CREATE TABLE IF NOT EXISTS `item` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gunsmith` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
   `price` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `gunsmith` (`gunsmith`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumpning av Data i tabell `item`
 --
 
-INSERT INTO `item` (`id`, `name`, `price`) VALUES
-(1, 'lock', 45),
-(2, 'stock', 30),
-(3, 'barrel', 25);
+INSERT INTO `item` (`id`, `gunsmith`, `name`, `price`) VALUES
+(1, 1, 'lock', 45),
+(2, 1, 'stock', 30),
+(3, 1, 'barrel', 25);
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `report`
+--
+
+CREATE TABLE IF NOT EXISTS `report` (
+  `salesperson` int(11) NOT NULL,
+  `month` date NOT NULL,
+  `salary` int(11) NOT NULL,
+  PRIMARY KEY (`salesperson`,`month`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -70,12 +85,30 @@ INSERT INTO `item` (`id`, `name`, `price`) VALUES
 CREATE TABLE IF NOT EXISTS `sale` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `salesperson` int(11) NOT NULL,
-  `town` int(11) NOT NULL, 
+  `town` int(11) NOT NULL,
   `item` int(11) NOT NULL,
   `date` date NOT NULL,
   `quantity` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`),
+  KEY `sale_ibfk_1` (`salesperson`),
+  KEY `sale_ibfk_2` (`town`),
+  KEY `sale_ibfk_3` (`item`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=37 ;
+
+--
+-- Dumpning av Data i tabell `sale`
+--
+
+INSERT INTO `sale` (`id`, `salesperson`, `town`, `item`, `date`, `quantity`) VALUES
+(1, 2, 2, 1, '2000-01-01', 3),
+(2, 2, 2, 2, '2000-01-01', 4),
+(3, 2, 2, 3, '2000-01-01', 5),
+(4, 3, 2, 1, '2000-01-01', 3),
+(5, 3, 2, 2, '2000-01-01', 4),
+(6, 3, 2, 3, '2000-01-01', 5),
+(7, 2, 4, 1, '2000-01-01', 11),
+(8, 2, 4, 2, '2000-01-01', 11),
+(9, 2, 4, 3, '2000-01-01', 11);
 
 -- --------------------------------------------------------
 
@@ -108,7 +141,7 @@ CREATE TABLE IF NOT EXISTS `town` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumpning av Data i tabell `town`
@@ -152,6 +185,18 @@ INSERT INTO `user` (`id`, `name`, `password`) VALUES
 --
 ALTER TABLE `gunsmith`
   ADD CONSTRAINT `gunsmith_ibfk_1` FOREIGN KEY (`id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restriktioner för tabell `item`
+--
+ALTER TABLE `item`
+  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`gunsmith`) REFERENCES `gunsmith` (`id`);
+
+--
+-- Restriktioner för tabell `report`
+--
+ALTER TABLE `report`
+  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`salesperson`) REFERENCES `salesperson` (`id`);
 
 --
 -- Restriktioner för tabell `sale`
