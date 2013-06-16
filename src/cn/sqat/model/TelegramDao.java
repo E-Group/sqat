ab93c9bcaf51d9de23039cdf899814491a339fe5
bauwie <davidbauwie@gmail.com>
2013-06-16 10:05:10 +0800

    Fixed inventory check before submitting telegram

--- src/cn/sqat/model/TelegramDao.java
+++ src/cn/sqat/model/TelegramDao.java
@@ -11,6 +11,10 @@ public class TelegramDao {
 
 	static Connection currentCon = null;
 	static ResultSet rs = null;
+	static final int MAX_LOCKS = 70;
+	static final int MAX_STOCKS = 80;
+	static final int MAX_BARRELS = 90;
+
 	public static TelegramBean submit(TelegramBean bean)
 	{
 		Statement stmt = null;
@@ -24,10 +28,53 @@ public class TelegramDao {
 
 			boolean reportExists = rs.next();
 			if (reportExists){
-				throw new IllegalStateException();
+				throw new IllegalStateException("Specified month is already reported!<br>");
+			}
+
+		} catch (SQLException e) {
+			System.out.println("DB failed: An Exception has occurred! " + e);
+		}
+
+		try{
+			stmt = currentCon.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from sale where date like '"+bean.getDate().substring(0,7)+"%' and salesperson="+bean.getId()+";");
+			int locks = 0;
+			int stocks = 0;
+			int barrels = 0;
+
+			while (rs.next()){
+				int item = rs.getInt("item");
+				int quantity = rs.getInt("quantity");
+				
+				switch(item){
+				case 1: locks += quantity; 
+				break;
+				case 2: stocks += quantity; 
+				break;
+				case 3: barrels += quantity; 
+				break;
+				}
+			}
+			StringBuilder str = new StringBuilder();
+			
+			if((bean.getLocks()+locks) > MAX_LOCKS){
+//				throw new IllegalStateException("Too many locks inserted, you have "+(MAX_LOCKS-locks)+" left in your inventory for this month.");
+				str.append("Too many locks inserted, you have "+(MAX_LOCKS-locks)+" left in your inventory for this month.<br>");
+			}
+			if ((bean.getStocks()+stocks) > MAX_STOCKS){
+//				throw new IllegalStateException("Too many stocks inserted, you have "+(MAX_STOCKS-stocks)+" left in your inventory for this month.");
+				str.append("Too many stocks inserted, you have "+(MAX_STOCKS-stocks)+" left in your inventory for this month.<br>");
+			}
+			if ((bean.getBarrels()+barrels) > MAX_BARRELS){
+				str.append("Too many barrels inserted, you have "+(MAX_BARRELS-barrels)+" left in your inventory for this month.<br>");
+//				throw new IllegalStateException("Too many barrels inserted, you have "+(MAX_BARRELS-barrels)+" left in your inventory for this month.");
 			}
 			
-
+			if(!str.toString().isEmpty()){
+				System.out.println(str.toString());
+				throw new IllegalStateException(str.toString());
+			}
+			
 		} catch (SQLException e) {
 			System.out.println("DB failed: An Exception has occurred! " + e);
 		}
